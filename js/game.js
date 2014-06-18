c516d7151e94a609b64c7d59277d535714565d5e
Daphne <daphnemiedema@hotmail.com>
2014-06-18 11:32:35 +0200

    Added music and markers, commented out because of http error

--- js/game.js
+++ js/game.js
@@ -74,6 +74,20 @@ function preload()
     this.load.image('npc5_rpg', 'assets/npc_05.png');
     this.load.image('npc6_rpg', 'assets/npc_06.png');
     this.load.image('npc7_rpg', 'assets/npc_07.png');
+
+    //Preload music
+    /*this.music = Phaser.Sound;
+    this.load.audio('music', ['assets/sounds.mp3'], false);
+    this.music = this.add.audio('music');
+    music.override = true;
+
+    music.addMarker('bg1', 0.028, 10.216, 1, true);
+    music.addMarker('bg2', 10.244, 10.214, 1, true);
+    music.addMarker('bg3', 20.458, 10.238, 1, true);
+    music.addMarker('bg4', 30.696, 10.188, 1, true);
+    music.addMarker('chat', 40.884, 0.314, 1, false);
+    music.addMarker('itempickup', 41.198, 0.494, 1, false);
+    music.addMarker('8bbg1', 41.629, 7.696, 1, true);*/
 };
 
 function create()
@@ -85,6 +99,7 @@ function create()
     this.state.add('dungeon', dungeonPhase);
     this.state.add('rpg', rpgPhase);
     this.state.add('evolution', evolution);
+
 
     //and start the menu one
     this.state.start('menu');
