f1847ab10b67e560a949e0bf2ace40946748a67c
Pieter Kokx <pieter@kokx.nl>
2014-06-18 09:40:53 +0200

    Added basic orange box collision.

--- js/npc.js
+++ js/npc.js
@@ -9,6 +9,9 @@ NPC = function(game, scale, sprite) {
     this.scale.setTo(scale, scale);
 
     this._hasOrangeBox = false;
+    this._orangebox = undefined;
+    this._orangegroup =  new Phaser.Group(this._game, null, "orangebox", false);
+    this._player = undefined;
 }
 
 NPC.prototype = Object.create(Phaser.Sprite.prototype);
@@ -18,27 +21,35 @@ NPC.prototype.constructor = NPC;
  * Automatically called by World.update
  */
 NPC.prototype.update = function() {
-    // todo: update
+    // collide with orangebox
+    if (this._hasOrangeBox) {
+        try {
+            this._game.physics.arcade.collide(this._player, this._orangegroup, function() {
+                console.log("aapjes");
+            }, null, this);
+        } catch (e) {}
+    }
 }
 
+
 NPC.prototype.spawnOrangeBox = function(player, npc) {
-    if (this._hasOrangeBox) {
+    if (npc._hasOrangeBox) {
         // we are still looking for it
         console.log("exists");
     } else {
+        // hacks
+        npc._player = player;
         // spawn 'orange box'
         var spawnPosition = player._game.state.getCurrentState().getItemPosition();
 
-        var orangebox = player._game.add.sprite(spawnPosition[0], spawnPosition[1], 'orange_box');
-        player._game.physics.enable(orangebox);
-        orangebox.body.immovable = true;
+        npc._orangebox = player._game.add.sprite(spawnPosition[0], spawnPosition[1], 'orange_box');
+        player._game.physics.enable(npc._orangebox);
+        npc._orangebox.body.immovable = true;
 
-        var orangegroup = new Phaser.Group(player._game, null, 'orangebox', false);
+        npc._orangegroup.add(npc._orangebox);
 
-        orangegroup.add(orangebox);
+        player._game.add.existing(npc._orangegroup);
 
-        player._game.add.existing(orangegroup);
-
-        this._hasOrangeBox = true;
+        npc._hasOrangeBox = true;
     }
 }
